8dc59139bbebab71520ed463539d8ee7855443f9
JulianaDu <julianapoisk@gmail.com>
2023-09-09 00:40:41 -0300

    ajuste em migrations (links e tags - tabelas recriadas), ainda com erro de servidor no insomnia

--- src/database/knex/migrations/20230905180611_createTags.js
+++ src/database/knex/migrations/20230905180611_createTags.js
@@ -2,13 +2,13 @@
 exports.up = knex => knex.schema.createTable("tags", table => {
   table.increments("id");
   table.text("name").notNullable(); //text é o tipo texto / notNullable significa que não é para aceitar valores nulos
+  table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE"); // significa que se eu deletar a nota a qual a tag estiver vinculada,
+  // automaticamente vai deletar a tag. Ou seja, deleta as tags que estão vinculadas à nota, em cascata.
   
   table.integer("user_id").references("id").inTable("users"); // aqui estou criando um campo do tipo inteiro na minha tabela, 
   //chamado "user_id" e ele faz uma referência ao "id" que existe dentro da tabela "users". 
   //Logo só posso criar uma nota se eu tiver um usuário para vinculá-la.
 
-  table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE"); // significa que se eu deletar a nota a qual a tag estiver vinculada,
-  // automaticamente vai deletar a tag. Ou seja, deleta as tags que estão vinculadas à nota, em cascata.
 
 }); //up cria a tabela
 
--- src/database/knex/migrations/20230905182448_createLinks.js
+++ src/database/knex/migrations/20230905182448_createLinks.js
@@ -1,10 +1,7 @@
 
 exports.up = knex => knex.schema.createTable("links", table => {
   table.increments("id");
-  table.integer("url").references("id").inTable("users"); // aqui estou criando um campo do tipo inteiro na minha tabela, 
-  //chamado "user_id" e ele faz uma referência ao "id" que existe dentro da tabela "users". 
-  //Logo só posso criar uma nota se eu tiver um usuário para vinculá-la.
-
+  table.text('url').notNullable();
   table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE"); // significa que se eu deletar a nota a qual a tag estiver vinculada,
   // automaticamente vai deletar os links. Ou seja, deleta os links que estão vinculados à nota, em cascata.
 
