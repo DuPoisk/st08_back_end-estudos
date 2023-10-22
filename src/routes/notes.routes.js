f848685344619c4f14b517f416e9ca3351088f0a
Ju Du <julianapoisk@gmail.com>
2023-10-22 16:56:11 -0300

    st08 Listando notas

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -6,6 +6,7 @@ const notesRoutes = Router();
 
 const notesController = new NotesController(); //a constante notesController é uma instância do NotesController
 
+notesRoutes.get("/", notesController.index);
 notesRoutes.post("/:user_id", notesController.create);
 notesRoutes.get("/:id", notesController.show); /* essa linha faz com que a rota seja visível*/
 notesRoutes.delete("/:id", notesController.delete); /* essa linha faz com que a rota de deletar seja visível*/
