4389187670f8f57fd38b3670b7712224b5ec9b0a
Ju DuPoisk <julianapoisk@gmail.com>
2023-10-26 01:07:01 -0300

    st10-1.5-Validando senha

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,5 +1,6 @@
 const knex = require("../database/knex");
-const AppError = require("../utils/AppError");
+const AppError = require("../utils/AppError"); // para validar se usuário existe
+ const {compare} = require("bcryptjs"); // para comparar a senha criptografada. "compare" é uma função dentro do bcryptjs
 
 
 class SessionsController {
@@ -8,10 +9,15 @@ class SessionsController {
 
     const user = await knex("users").where({email}).first(); // na tabela users, quero filtrar o usuário pelo emmail. first() serve para garantir que devolva somente UM.
     
-    if(!user) { // "se usuário nao existir"
-      throw new AppError("Senha e/ ou e-mail in, 401"); // o erro 401 significa não autorizado
+    if(!user) { // "SE USUÁRIO NÃO EXISTIR"
+      throw new AppError("Senha e/ ou e-mail incorreto", 401); // o erro 401 significa não autorizado
     }
-
+    
+    const passwordMatched = await compare(password, user.password); // comparo a senha que o usuário acabou de digitar, com a senha cadastrada no banco de dados
+    
+    if (!passwordMatched) { //"SE SENHA FOR INCORRETA". se for falso, significa que não bateu a senha. Então mostro o erro: 
+      throw new AppError("Senha e/ ou e-mail incorreto", 401); // o erro 401 significa não autorizado
+    }
     
     return response.json({user});
   }
