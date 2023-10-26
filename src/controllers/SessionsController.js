cea9526efb4c1453761bf55907afb57e3443497b
Ju DuPoisk <julianapoisk@gmail.com>
2023-10-26 00:58:14 -0300

    st10-aula_1.4-validando usuário cadastrado

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,7 +1,19 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+
+
 class SessionsController {
   async create(request, response){ //assíncrona, pois envolve conexão com banco de dados. crio aqui uma sessão para o usuário.
     const {email, password} = request.body;
-    return response.json({email, password});
+
+    const user = await knex("users").where({email}).first(); // na tabela users, quero filtrar o usuário pelo emmail. first() serve para garantir que devolva somente UM.
+    
+    if(!user) { // "se usuário nao existir"
+      throw new AppError("Senha e/ ou e-mail in, 401"); // o erro 401 significa não autorizado
+    }
+
+    
+    return response.json({user});
   }
 }
 
