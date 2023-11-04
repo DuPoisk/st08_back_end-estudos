e72788ee0ab5e96d622c29847376d29881fee693
Ju DuPoisk <julianapoisk@gmail.com>
2023-11-04 19:30:04 -0300

    st11-Testes-11-(com erro)Users Repository in Memory

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,16 +1,16 @@
 const UserCreateService = require("./UserCreateService");
+const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory");
 
-it ("user should be creat", () => { // it recebe 2 parâmetros: 1=descrição do teste,com a expectativa de resultado; 2=função que vai executar de fato  o teste
+it ("user should be create", async () => { // it recebe 2 parâmetros: 1=descrição do teste,com a expectativa de resultado; 2=função que vai executar de fato  o teste
   const user = {
     name: "User Test",
     email: "user@test.com",
     password: "123"
   }; // exemplo de dados eviados de uma requisição pelo usuário
 
-  const  userCreateService = new UserCreateService();
+  const userRepositoryInMemory = new UserRepositoryInMemory();
+  const userCreateService = new UserCreateService(userRepositoryInMemory);
   const userCreated = await userCreateService.execute(user);
 
   expect(userCreated).toHaveProperty("id");
-
-
 }); 
